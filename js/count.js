807345210d64e4b8a8a5a682f1b869e8d056453f
Rihanul Islam <rihanthedev@gmail.com>
2023-04-07 05:13:55 +0600

    commited

--- js/count.js
+++ js/count.js
@@ -3,7 +3,7 @@ let value = 0;
 document.getElementById("blue-btn").addEventListener("click", function () {
   //   value = value + 1;
   value += 1;
-
+  // const localStorge = localStorage.setItem()
   //   console.log(value);
   document.getElementById("counter").innerText = value;
 });
